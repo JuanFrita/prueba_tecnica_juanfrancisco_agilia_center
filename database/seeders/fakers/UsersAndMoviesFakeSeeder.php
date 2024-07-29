c3a3c6e1dd89c765156dfa5ce8fb163ffe2f0fba
juanfran sanchez <erjuanfri01@gmail.com>
2024-07-29 19:28:48 +0200

    Ajustes seeder de usuarios

--- database/seeders/fakers/UsersAndMoviesFakeSeeder.php
+++ database/seeders/fakers/UsersAndMoviesFakeSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders\Fakers;
 use App\Models\Category;
 use App\Models\Movie;
 use App\Models\User;
+use Hash;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class UsersAndMoviesFakeSeeder extends Seeder
@@ -15,7 +17,16 @@ class UsersAndMoviesFakeSeeder extends Seeder
     public function run(): void
     {
         $categories = Category::all();
-        User::factory()->count(5)->create()->each(function ($user) use ($categories) {
+        $password = Hash::make('1234');
+        User::factory()->count(5)->state(
+            new Sequence(
+                ['email' => 'fake1@fakemail.com'],
+                ['email' => 'fake2@fakemail.com'],
+                ['email' => 'fake3@fakemail.com'],
+                ['email' => 'fake4@fakemail.com'],
+                ['email' => 'fake5@fakemail.com'],
+            )
+        )->create(['password' => $password])->each(function ($user) use ($categories) {
             Movie::factory(10)->create(['user_id' => $user->id])->each(function ($movie) use ($categories) {
                 $movie->categories()->attach(
                     $categories->random(2)->pluck('id')->toArray()
